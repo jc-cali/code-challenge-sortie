de8e03ea0e8a013b7a03c27c39826f22ed97382f
Jeff Chin <dizaburrow@gmail.com>
2016-08-16 16:01:29 -0500

    Complete

--- app/src/main/java/org/codeforcoffee/sortie/MainActivity.java
+++ app/src/main/java/org/codeforcoffee/sortie/MainActivity.java
@@ -1,12 +1,23 @@
 package org.codeforcoffee.sortie;
 
+import android.content.Context;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
+import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,14 +46,19 @@ public class MainActivity extends AppCompatActivity {
         mFamousPetsList.add("squirtle");
 
 
-        mListView = (ListView) findViewById(R.id.list_view);
 
         // sort the arrayList descendingly
 
         ArrayList<String> displayData = sortDescending(mFamousPetsList);
 
         // render the list using the android.R.simple_list_row_1 layout
+        ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mFamousPetsList);
+        // Set The Adapter
 
+        ListView mListView = (ListView) findViewById(R.id.list_view);
+
+        mListView.setAdapter(arrayAdapter);
 
 
     }
@@ -51,6 +67,21 @@ public class MainActivity extends AppCompatActivity {
 
         // sort the arrayList descending alphabetically
         // do not use ArrayList.sort()
+        for (int i = 0; i < list.size(); i++) {
+
+            for (int a = list.size() - 1; a > i; a--) {
+                String x = list.get(i).toLowerCase();
+                String y = list.get(a).toLowerCase();
+
+                int compare = x.compareTo(y);
+                    // Check if second entry is before first entry and swap
+                    if (compare > 0) {
+                        String tmp = list.get(i);
+                        list.set(i,list.get(a)) ;
+                        list.set(a,tmp);
+                    }
+                }
+         }
 
         return list;
 
